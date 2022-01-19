0b00b9a4c158234d82d57028b65c293439e14920
Anthony Chung <anthonycj04@gmail.com>
2022-01-19 19:30:46 +0800

    script for merkelroot

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -43,6 +43,38 @@ task("verify_primate_weapon", "verify PrimateWeapon")
     });
   });
 
+task("set_primate_weapon_root", "set PrimateWeapon merkleroot")
+  .addPositionalParam("address", "address", undefined, types.string, false)
+  .addPositionalParam("root", "root", undefined, types.string, false)
+  .setAction(async ({ address, root }, hre) => {
+    const w = await hre.ethers.getContractAt("PrimateWeapon", address);
+    const tx = await w.setPreSaleRoot(root, {
+      maxFeePerGas: hre.ethers.utils.parseUnits("2", "gwei"),
+      maxPriorityFeePerGas: hre.ethers.utils.parseUnits("1", "gwei"),
+    });
+    console.log(tx);
+  });
+
+task("presale_mint_primate_weapon", "set PrimateWeapon merkleroot")
+  .addPositionalParam("address", "address", undefined, types.string, false)
+  .addPositionalParam("amount", "amount", undefined, types.int, false)
+  .setAction(async ({ address, amount }, hre) => {
+    const w = await hre.ethers.getContractAt("PrimateWeapon", address);
+    const proof = [
+      "0x859149f803fc0798c28b8529855eeba7ca1a499f4216ab98feeb0a26df1e7e74",
+      "0x2a076ea347f8b245f7cfcb81c7edd12715ccbff52cc5ab38e07135cd809e1337",
+      "0xfec1b64f2adcd7192aca9fcb49283b53e75391459cc046d2866fdb71847d2239",
+    ];
+    const _amount = hre.ethers.BigNumber.from(amount);
+    const price = await w.PRE_SALE_PRICE();
+    const tx = await w.preSaleMint(_amount, 54, proof, {
+      value: _amount.mul(price),
+      maxFeePerGas: hre.ethers.utils.parseUnits("2", "gwei"),
+      maxPriorityFeePerGas: hre.ethers.utils.parseUnits("1", "gwei"),
+    });
+    console.log(tx);
+  });
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
---
+++ scripts/buildMerkelTree.ts
@@ -0,0 +1,78 @@
+// import { ethers } from "hardhat";
+import { writeFileSync } from "fs";
+import { BigNumber, BigNumberish } from "ethers";
+import { MerkleTree } from "merkletreejs";
+// doesn't work with merkletreejs if imported from ethers/lib/utils
+import keccak256 from "keccak256";
+import { solidityKeccak256, getAddress } from "ethers/lib/utils";
+
+interface ClaimEntry {
+  amount: string;
+  proof: string[];
+}
+
+const hashEntry = (addr: string, amount: BigNumberish) => {
+  return solidityKeccak256(["address", "uint256"], [addr, amount]);
+};
+
+const formatBNForOutput = (bn: BigNumber) => {
+  return bn.toHexString();
+  //   return bn.toString();
+};
+
+function buildMerkleTree(
+  distributions: Map<string, BigNumber>,
+  claimEntries: Map<string, ClaimEntry>
+) {
+  const leaves = new Map<string, string>();
+  for (const [addr, amount] of distributions) {
+    const leaf = hashEntry(addr, amount);
+    leaves.set(addr, leaf);
+  }
+  const tree = new MerkleTree(Array.from(leaves.values()), keccak256, {
+    sort: true,
+  });
+  for (const [addr, amount] of distributions) {
+    claimEntries.set(addr, {
+      amount: formatBNForOutput(amount),
+      proof: tree.getHexProof(leaves.get(addr)!),
+    } as ClaimEntry);
+  }
+  return tree.getHexRoot();
+}
+
+async function main() {
+  const data = [
+    ["0x5C0Eb280f5CE42C34E196D760A6a223e141554a0", BigNumber.from(50)],
+    ["0xb63983f0E62039f77b117e81763941BbEa6cBa58", BigNumber.from(51)],
+    ["0xa0C64dd1Fd4B597BeC285D32eDAffa9E7b2316Bb", BigNumber.from(52)],
+    ["0xd31Ee6b54282cA928e2B5302aDd6FAB75a93605f", BigNumber.from(53)],
+    ["0xbab4E996079c65924Db19Bd93d8C11bdB5f75Da2", BigNumber.from(54)],
+  ] as [string, BigNumber][];
+
+  const distributions = new Map<string, BigNumber>();
+  let actualDistribution = BigNumber.from(0);
+  for (const [addr, amount] of data) {
+    const a = getAddress(addr);
+    if (distributions.has(a)) {
+      throw Error("duplicate distribution");
+    }
+    distributions.set(a, amount);
+    actualDistribution = actualDistribution.add(amount);
+  }
+  const claims = new Map<string, ClaimEntry>();
+  const merkleRoot = buildMerkleTree(distributions, claims);
+  writeFileSync(
+    "claims.json",
+    JSON.stringify({
+      merkleRoot: merkleRoot,
+      totalAmount: formatBNForOutput(actualDistribution),
+      claims: Object.fromEntries(claims),
+    })
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
